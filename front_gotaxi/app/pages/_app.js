f3d3aa20198c8d6385ebb7b216cb9a4e1896b755
Hectorherrerautp <hectorh23999@gmail.com>
2025-07-23 13:33:59 -0500

    reinicio

--- front_gotaxi/app/pages/_app.js
+++
@@ -1,7 +0,0 @@
-// pages/_app.js
-import '../styles/style.css';
-import '../styles/globals.css';  // si usas también el global de Tailwind u otro
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
